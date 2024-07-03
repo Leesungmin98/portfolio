d132f9fe9ada952d90e96518d9c00b14625f97cb
Leesungmin98 <placidus00@gmail.com>
2024-07-03 18:43:08 +0900

    ㅇㄴㄹㄴㅇㅇ

--- js/app.js
+++ js/app.js
@@ -1,50 +1,50 @@
-// // app.js
-// import { Wave } from './wave.js';
+// app.js
+import { Wave } from './wave.js';
 
-// class App {
-//   constructor() {
-//     this.canvas = document.createElement('canvas');
-//     this.ctx = this.canvas.getContext('2d');
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
 
-//     const waveContainer = document.querySelector('.wave');
-//     waveContainer.appendChild(this.canvas);
+    const waveContainer = document.querySelector('.wave');
+    waveContainer.appendChild(this.canvas);
 
-//     this.waves = [
-//       new Wave('#93E4FF', -600, 1, 0.02),
-//       new Wave('#93E4FF', -1000, 0.5, 0.01),
-//     //   new Wave('#93E4FF', -1600, 0.2, 0.005),
-//     ];
+    this.waves = [
+      new Wave('#93E4FF', -600, 1, 0.02),
+      new Wave('#93E4FF', -1000, 0.5, 0.01),
+    //   new Wave('#93E4FF', -1600, 0.2, 0.005),
+    ];
 
-//     window.addEventListener('resize', this.resize.bind(this));
-//     this.resize();
-//     requestAnimationFrame(this.animate.bind(this));
-//   }
+    window.addEventListener('resize', this.resize.bind(this));
+    this.resize();
+    requestAnimationFrame(this.animate.bind(this));
+  }
 
-//   resize() {
-//     this.stageWidth = document.body.clientWidth;
-//     this.stageHeight = document.body.clientHeight;
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
 
-//     this.canvas.width = this.stageWidth * 2;
-//     this.canvas.height = this.stageHeight * 2;
-//     this.ctx.scale(2, 2);
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
 
-//     this.waves.forEach(wave => {
-//       wave.resize(this.stageWidth, this.stageHeight);
-//     });
-//   }
+    this.waves.forEach(wave => {
+      wave.resize(this.stageWidth, this.stageHeight);
+    });
+  }
 
-//   animate(t) {
-//     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  animate(t) {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-//     this.waves.forEach(wave => {
-//       wave.update(t * 0.002);
-//       wave.draw(this.ctx);
-//     });
+    this.waves.forEach(wave => {
+      wave.update(t * 0.002);
+      wave.draw(this.ctx);
+    });
 
-//     requestAnimationFrame(this.animate.bind(this));
-//   }
-// }
+    requestAnimationFrame(this.animate.bind(this));
+  }
+}
 
-// window.onload = () => {
-//   new App();
-// };
+window.onload = () => {
+  new App();
+};
--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 $(function() {
-    // 각 스킬 항목을 클릭하면 해당 모달을 엽니다.
+/// 스킬 
+
+    // 모달 열기
     $('.skill-item').click(function() {
         var modalClass = $(this).attr('class').split(' ')[1] + '-modal';
         $('.' + modalClass).removeClass('hidden');
     });
 
-    // 닫기 버튼을 클릭하면 모든 모달을 닫습니다.
+    // 닫기 버튼
     $('.close').click(function() {
         $(this).closest('.modal').addClass('hidden');
     });
@@ -13,81 +15,121 @@ $(function() {
 
 
 //////////////////////////////////////////////////////////////////////////////
+// 포트폴리오-디자인
 
 $(document).ready(function() {
     
+    // 클릭 이벤트
     $('.info-box').click(function() {
-        $('.close-modal').css('display', 'block');
-        // po-modal-box를 화면에 보이도록 함
-        $('.info-modal-box').css('bottom', '0');
-        $('body').css('overflow', 'hidden'); // 스크롤 금지
+        openModal('.info-modal-box');
     });
-    $('.close-modal').click(function() {
-        $('.close-modal').css('display', 'none');
-        // po-modal-box를 화면 아래로 내리는 애니메이션
-        $('.info-modal-box').css('bottom', '-100%');
-        $('body').css('overflow', 'auto'); // 스크롤 허용
-    });
-
 
     $('.logo-box').click(function() {
-        // po-modal-box를 화면에 보이도록 함
-        $('.logo-modal-box').css('bottom', '0');
-        $('body').css('overflow', 'hidden'); // 스크롤 금지
-    });
-    $('.close-modal').click(function() {
-        // po-modal-box를 화면 아래로 내리는 애니메이션
-        $('.logo-modal-box').css('bottom', '-100%');
-        $('body').css('overflow', 'auto'); // 스크롤 허용
+        openModal('.logo-modal-box');
     });
 
     $('.mobile-box').click(function() {
-        // po-modal-box를 화면에 보이도록 함
-        $('.mobile-modal-box').css('bottom', '0');
-        $('body').css('overflow', 'hidden'); // 스크롤 금지
-    });
-    $('.close-modal').click(function() {
-        // po-modal-box를 화면 아래로 내리는 애니메이션
-        $('.mobile-modal-box').css('bottom', '-100%');
-        $('body').css('overflow', 'auto'); // 스크롤 허용
+        openModal('.mobile-modal-box');
     });
 
     $('.package-box').click(function() {
-        // po-modal-box를 화면에 보이도록 함
-        $('.mobile-modal-box').css('bottom', '0');
-        $('body').css('overflow', 'hidden'); // 스크롤 금지
-    });
-    $('.close-modal').click(function() {
-        // po-modal-box를 화면 아래로 내리는 애니메이션
-        $('.package-modal-box').css('bottom', '-100%');
-        $('body').css('overflow', 'auto'); // 스크롤 허용
+        openModal('.package-modal-box');
     });
 
     $('.detail-box').click(function() {
-        $('.close-modal').css('display', 'block');
-        // po-modal-box를 화면에 보이도록 함
-        $('.mobile-modal-box').css('bottom', '0');
-        $('body').css('overflow', 'hidden'); // 스크롤 금지
-    });
-    $('.close-modal').click(function() {
-        $('.close-modal').css('display', 'none');
-        // po-modal-box를 화면 아래로 내리는 애니메이션
-        $('.detail-modal-box').css('bottom', '-100%');
-        $('body').css('overflow', 'auto'); // 스크롤 허용
+        openModal('.detail-modal-box');
     });
 
     $('.banner-box').click(function() {
-        // po-modal-box를 화면에 보이도록 함
-        $('.banner-modal-box').css('bottom', '0');
-        $('body').css('overflow', 'hidden'); // 스크롤 금지
+        openModal('.banner-modal-box');
     });
+
+    // 닫기 버튼
     $('.close-modal').click(function() {
-        // po-modal-box를 화면 아래로 내리는 애니메이션
-        $('.banner-modal-box').css('bottom', '-100%');
-        $('body').css('overflow', 'auto'); // 스크롤 허용
+        closeModal();
+    });
+
+    // 모달 열기
+    function openModal(modalClass) {
+        $('.close-modal').css('display', 'block');
+        $(modalClass).css('bottom', '0');
+        $('body').css('overflow', 'hidden');
+    }
+
+    // 모달 닫기
+    function closeModal() {
+        $('.close-modal').css('display', 'none');
+        $('.info-modal-box, .logo-modal-box, .mobile-modal-box, .package-modal-box, .detail-modal-box, .banner-modal-box').css('bottom', '-100%');
+        $('body').css('overflow', 'auto');
+    }
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// 햄버거 메뉴
+$(function() {
+    $('#toggle').click(function() {
+        $(this).toggleClass('active');
+        $('#fullscreen').toggleClass('open');
+
+        // PC 버전이 아닌 경우에만 body의 오버플로우 스타일을 변경
+        if (!isPcVersion()) {
+            if (!$('#fullscreen').hasClass('open')) {
+                $('body').css('overflow', 'auto');
+            } else {
+                $('body').css('overflow', 'hidden');
+            }
+        }
+    });
+
+    // PC 버전인지 확인하는 함수
+    function isPcVersion() {
+        return window.innerWidth >= 768;
+    }
+
+    // mobile-menu의 링크를 클릭했을 때 fullscreen을 닫음
+    $('.mobile-menu a').click(function() {
+        $('#toggle').removeClass('active');
+        $('#fullscreen').removeClass('open');
+        $('body').css('overflow', 'auto'); // body의 overflow를 다시 auto로 설정
     });
 });
 
+///////////////////////////////////////////////////////////////////////
+// 스크롤 이동
 
-////////////////////////////////////////////////////////////////////
+$(function() {
+    const gnbA = $('.mobile-menu > li > a');
+    gnbA.click(function() {
+        
+        const target = $(this).attr('href'); // 메뉴를 클릭했을 때 해당 섹션의 ID (예: #m2)
+        $('html').animate({ scrollTop: $(target).offset().top }, 1000);
+        e.preventDefault();
+    });
 
+    const gnbB = $('.pc-menu > li > a');
+    gnbB.click(function() {
+        
+        const target = $(this).attr('href'); // 메뉴를 클릭했을 때 해당 섹션의 ID (예: #m2)
+        $('html').animate({ scrollTop: $(target).offset().top }, 1000);
+        e.preventDefault();
+    });
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+// 탑 버튼 
+$(function(){
+    // scrollTop() 의 위치가 200을 초과하면, top-bt가 서서히 드러남
+    // 200을 초과하지 않으면 서서히 사라짐.
+    $(window).scroll(function() { // 스크롤이 되면,
+         if ($(this).scrollTop() > 200) {
+            $('#top-bt').fadeIn();
+         } else {
+            $('#top-bt').fadeOut();
+         }
+    })
+    // top-bt를 클릭하면 scollTop의 위치가 0으로 부드럽게 문서가 이동
+    $('#top-bt').click(function(e) {
+        e.preventDefault(); // 버블링 금지
+        $('html').animate({ scrollTop:0 }, 300);
+    })
+});
