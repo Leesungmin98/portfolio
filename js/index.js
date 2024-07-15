b8c82b3be83556f807bb62d3b58eb91cfe54fe7a
Leesungmin98 <placidus00@gmail.com>
2024-07-15 16:07:43 +0900

    23423434

--- js/index.js
+++ js/index.js
@@ -28,7 +28,8 @@ $(function() {
 $(document).ready(function() {
     
   
-    $('.info-box').click(function() {
+     // 포트폴리오-디자인
+     $('.info-box').click(function() {
         openModal('.info-modal-box');
     });
 
@@ -66,6 +67,7 @@ $(document).ready(function() {
         $('.close-modal').css('display', 'block');
         $(modalClass).css('bottom', '0');
         $('body').css('overflow', 'hidden');
+        $('#top-bt').fadeOut(); 
     }
 
     // 모달 닫기
@@ -73,7 +75,66 @@ $(document).ready(function() {
         $('.close-modal').css('display', 'none');
         $('.info-modal-box, .character-modal-box, .logo-modal-box, .mobile-modal-box, .package-modal-box, .detail-modal-box, .banner-modal-box').css('bottom', '-100%');
         $('body').css('overflow', 'auto');
+        
+       
+        if ($(window).scrollTop() > 200) {
+            $('#top-bt').fadeIn();
+        }
     }
+
+    // 햄버거 메뉴
+    $('#toggle').click(function() {
+        $(this).toggleClass('active');
+        $('#fullscreen').toggleClass('open');
+        
+        if (!isPcVersion()) {
+            if (!$('#fullscreen').hasClass('open')) {
+                $('body').css('overflow', 'auto');
+            } else {
+                $('body').css('overflow', 'hidden');
+            }
+        }
+    });
+
+    // PC 버전
+    function isPcVersion() {
+        return window.innerWidth >= 768;
+    }
+
+    $('.mobile-menu a').click(function() {
+        $('#toggle').removeClass('active');
+        $('#fullscreen').removeClass('open');
+        $('body').css('overflow', 'auto');
+    });
+
+    // 스크롤 이동
+    const gnbA = $('.mobile-menu > li > a');
+    gnbA.click(function(e) {
+        const target = $(this).attr('href');
+        $('html').animate({ scrollTop: $(target).offset().top }, 1000);
+        e.preventDefault();
+    });
+
+    const gnbB = $('.pc-menu > li > a');
+    gnbB.click(function(e) {
+        const target = $(this).attr('href');
+        $('html').animate({ scrollTop: $(target).offset().top }, 1000);
+        e.preventDefault();
+    });
+
+    // 탑 버튼
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('#top-bt').fadeIn();
+        } else {
+            $('#top-bt').fadeOut();
+        }
+    });
+
+    $('#top-bt').click(function(e) {
+        e.preventDefault(); 
+        $('html').animate({ scrollTop:0 }, 300);
+    });
 });
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -139,7 +200,7 @@ $(function(){
     })
    
     $('#top-bt').click(function(e) {
-        e.preventDefault(); // 버블링 금지
+        e.preventDefault(); 
         $('html').animate({ scrollTop:0 }, 300);
     })
 });
